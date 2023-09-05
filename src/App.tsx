c1cbd28f851809836c3b8532a2bbc1034a710bdd
intemporalis <143558830+intemporalis@users.noreply.github.com>
2023-09-05 15:46:43 -0400

    build single view

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Menu from "./components/menu/Menu";
 import "./styles/global.scss"
+import Product from "./pages/product/Product.tsx";
+import User from "./pages/user/User.tsx";
 
 function App() {
 
@@ -47,6 +49,14 @@ function App() {
           {
             path: "/products",
             element: <Products/>
+          },
+          {
+            path: "/users/:id",
+            element: <User/>
+          },
+          {
+            path: "/products/:id",
+            element: <Product/>
           },
         ],
       },
